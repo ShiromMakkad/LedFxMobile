813b0973c01bf99c4321849f1ce72759055b09d2
Shirom Makkad <shirmak11@gmail.com>
2020-11-18 16:56:58 -0500

    Finished Navbar

--- App.js
+++ App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar'
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 import { ThemeProvider } from 'react-native-elements'
-import { View, SafeAreaView, Platform } from 'react-native';
+import { View, Platform } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faLightbulb, faBroadcastTower, faPlus } from '@fortawesome/free-solid-svg-icons'
 import Browser from './App/Browser'
+import Devices from './App/Devices'
+import Add from './App/Add'
 
 global.theme = {
   colors: {
-    primary: '#1D7DB9'
+    primary: '#1D7DB9',
+    secondary: '#212121',
+    success: '#3CB479',
+    error: '#F44336',
+    warning: '#BAA441',
   }
 }
 
@@ -18,15 +29,68 @@ const ColoredStatusBar = ({backgroundColor, ...props}) => (
     </View>
 );
 
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    <View>
-      <ColoredStatusBar backgroundColor={global.theme.colors.primary} style='light' />
-      <SafeAreaView style={{ flex: 1 }} forceInset={{ top: 'never', bottom: 'never' }}>
-        <ThemeProvider theme={global.theme}>
-          <Browser />
-        </ThemeProvider>
-      </SafeAreaView>
-    </View>
+    <ThemeProvider theme={global.theme}>
+      <ColoredStatusBar backgroundColor={global.theme.colors.secondary} style='light' />
+      <SafeAreaProvider>
+        <NavigationContainer theme={global.theme}>
+          <Tab.Navigator
+            initialRouteName="Browser"
+            screenOptions={{
+              cardStyle: {
+                backgroundColor: global.theme.colors.secondary
+              }
+            }}
+            tabBarOptions={{
+              activeTintColor: global.theme.colors.primary,
+              inactiveTintColor: '#ffffff',
+              safeAreaInsets: {top: 0, bottom: 7, right: 0, left: 0},
+              tabStyle: {
+                paddingTop: 7 
+              },
+              style: {
+                position: 'absolute',
+                backgroundColor: global.theme.colors.secondary,
+              }
+            }}
+          >
+            <Tab.Screen
+              name="Browser"
+              component={Browser}
+              options={{
+                tabBarLabel: 'Lights',
+                tabBarIcon: ({ color, size }) => (
+                  <FontAwesomeIcon icon={faLightbulb} size={size} color={color} />
+                ),
+              }} 
+            />
+            <Tab.Screen
+              name="Devices"
+              component={Devices}
+              options={{
+                tabBarLabel: 'Devices',
+                tabBarIcon: ({ color, size }) => (
+                  <FontAwesomeIcon icon={faBroadcastTower} size={size} color={color} />
+                ),
+              }} 
+            />
+            <Tab.Screen
+              name="Add"
+              component={Add}
+              options={{
+                tabBarLabel: 'Add Device',
+                tabBarIcon: ({ color, size }) => (
+                  <FontAwesomeIcon icon={faPlus} size={size} color={color} />
+                ),
+              }} 
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
+
--- App/Add.js
+++ App/Add.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function Browser() {
+export default function Add() {
     return (
         <SafeAreaView>
             <View>
-                <Text>Test</Text>
+                <Text>Test Add</Text>
             </View>
         </SafeAreaView>
     )
