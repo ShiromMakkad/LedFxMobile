9caf9e3f0954ba991cae06651cace07266fb986b
Shirom Makkad <shirmak11@gmail.com>
2020-11-18 13:22:58 -0500

    Fixed Status Bar

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { ThemeProvider } from 'react-native-elements'
+import { View, SafeAreaView, Platform } from 'react-native';
+import Browser from './App/Browser'
+
+global.theme = {
+  colors: {
+    primary: '#1D7DB9'
+  }
+}
+
+//This ensures that there is a single color status bar on both IOS and Android and that the content starts below it. 
+const ColoredStatusBar = ({backgroundColor, ...props}) => (
+    <View style={{height: getStatusBarHeight(true), backgroundColor: backgroundColor}}>
+        <StatusBar translucent={Platform.OS === 'ios' ? true : false} backgroundColor={backgroundColor} {...props} />
+    </View>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <ColoredStatusBar backgroundColor={global.theme.colors.primary} style='light' />
+      <SafeAreaView style={{ flex: 1 }} forceInset={{ top: 'never', bottom: 'never' }}>
+        <ThemeProvider theme={global.theme}>
+          <Browser />
+        </ThemeProvider>
+      </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
